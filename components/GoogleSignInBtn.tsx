3e2421ce55f594954792976a4c74121bf96f7e3c
Oliver Wu <97377068+MUOliver1026@users.noreply.github.com>
2023-09-20 17:20:10 +1000

    Add files via upload

---
+++ components/GoogleSignInBtn.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactNode } from "react"
+import { Button } from "./ui/button"
+
+interface GoogleSignInBtnProps {
+    children: ReactNode
+}
+
+const GoogleSignInBtn: FC<GoogleSignInBtnProps> = ({ children }) => {
+    const loginWithGoogle = () => {
+        console.log("login with google")
+    }
+
+    return (
+        <Button onClick={loginWithGoogle} className="w-full">
+            {children}
+        </Button>
+    )
+}
+
+export default GoogleSignInBtn
