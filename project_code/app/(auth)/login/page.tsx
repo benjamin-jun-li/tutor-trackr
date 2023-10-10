55194f4363023e04a2d2edc22f32fc10afd6ee57
Sammyci <cidawang24@gmail.com>
2023-10-10 21:50:02 +1100

    UOOI-18 Update resetpassword page

--- project_code/app/(auth)/login/page.tsx
+++ project_code/app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
     <section className="max-w-[18rem] mt-4">
         <SignInForm />
         <p className="mt-4 text-center">
-            <a href="/password-reset" className="text-blue-500 hover:underline">Forget your password?</a>
+            <a href="/reset-password" className="text-blue-500 hover:underline">Forget your password?</a>
         </p>
     </section>
   )
