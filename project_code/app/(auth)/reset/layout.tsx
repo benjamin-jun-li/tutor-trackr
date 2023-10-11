8f7aba8184564c4ea32812669bb0874855760557
Jun Li <benjaminli060@gmail.com>
2023-10-11 16:21:24 +1100

    UOOI-18 added detail functionality structure

--- project_code/app/(auth)/reset/layout.tsx
+++ project_code/app/(auth)/reset/layout.tsx
@@ -1,10 +1,10 @@
 import { FC, ReactNode } from 'react'
 
-interface ForgetLayoutProps {
+interface ResetLayoutProps {
     children: ReactNode
 }
 
-const ForgetLayout: FC<ForgetLayoutProps> = ({ children }) => {
+const ResetLayout: FC<ResetLayoutProps> = ({ children }) => {
     return (
         <main className='min-h-screen bg-slate-200 p-10 pt-24 rounded-md flex flex-col justify-center items-center'>
             {children}
@@ -12,4 +12,4 @@ const ForgetLayout: FC<ForgetLayoutProps> = ({ children }) => {
     )
 }
 
-export default ForgetLayout
+export default ResetLayout
