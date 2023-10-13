3403b40ed7df6c2161b73cf14e600d954cccb4b6
Oliver Wu <z5379238@ad.unsw.edu.au>
2023-10-13 17:38:21 +1100

    mainpage admin login

--- project_code/components/Navbar.tsx
+++ project_code/components/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
     if (isAdminPage || isTutorPage || isStudentPage) {
       return <UserNav userName={userName} userEmail={userEmail} />;
     }
-    return <></>
+    return <Link href="/admin/login" className="underline text-blue-500 hover:text-black">Admin</Link>
   };
 
   const handleLogoLink = (url: string) => {
