939bcc3753eca527485527f47006166c6e53798e
Oliver Wu <z5379238@ad.unsw.edu.au>
2023-10-28 22:04:50 +1100

    UOOI-54 tutor message box details

--- project_code/app/tutor/message/[id]/page.tsx
+++ project_code/app/tutor/message/[id]/page.tsx
@@ -2,16 +2,20 @@
 
 import React from 'react';
 import { useParams } from "next/navigation";
+import Link from "next/link";
 
 const MessagePage = () => {
     const params = useParams();
 
     return (
-        <main className="mt-20">
+        <main className="flex flex-col justify-center items-center">
             <h1 className="text-2xl">
                 Message Page
             </h1>
             <p>Message: {params?.id}</p>
+            <Link href="/tutor/dashboard">
+                <button className="px-4 py-2 mt-4 text-white bg-blue-600 rounded-md hover:bg-blue-700">Return</button>
+            </Link>
         </main>
 
     );
