3a41cae447e5eb8a0660282a0be3136ac7393e3c
dior <cidawang24@gmail.com>
2023-10-28 17:38:44 +1100

    update fileter logic

--- project_code/components/dashboard/course-filter.tsx
+++ project_code/components/dashboard/course-filter.tsx
@@ -1,36 +1,41 @@
 import Combobox from "@/components/ui/combo-box";
 
-const frameworks = [
-    {
-        value: "next.js",
-        label: "Next.js",
-    },
-    {
-        value: "sveltekit",
-        label: "SvelteKit",
-    },
-    {
-        value: "nuxt.js",
-        label: "Nuxt.js",
-    },
-    {
-        value: "remix",
-        label: "Remix",
-    },
-    {
-        value: "astro",
-        label: "Astro",
-    },
-]
+const timeZones = [
+    { value: "utc", label: "UTC" },
+    { value: "est", label: "Eastern Standard Time" },
+];
+
+const courseTypes = [
+    { value: "web", label: "Web Development" },
+    { value: "data", label: "Data Science" },
+];
+
+const experienceLevels = [
+    { value: "beginner", label: "Beginner" },
+    { value: "intermediate", label: "Intermediate" },
+    { value: "expert", label: "Expert" },
+];
+
+const courseCounts = [
+    { value: "1-10", label: "1-10 courses" },
+    { value: "11-50", label: "11-50 courses" },
+];
+
+const tutorRates = [
+    { value: "10-20", label: "$10-$20/hr" },
+    { value: "21-50", label: "$21-$50/hr" },
+];
 
 const CourseFilter = () => {
     return (
-        <section>
-            <Combobox items={frameworks}/>
-            <Combobox items={frameworks}/>
-            <Combobox items={frameworks}/>
+        <section style={{ display: 'flex'}}>
+            <Combobox items={timeZones} placeholder="Select Time Zone" />
+            <Combobox items={courseTypes} placeholder="Select Course Type" />
+            <Combobox items={experienceLevels} placeholder="Select Tutor Experience Level" />
+            <Combobox items={courseCounts} placeholder="Select Tutor Course Count" />
+            <Combobox items={tutorRates} placeholder="Select Tutor Rate" />
         </section>
-    )
+    );
 }
 
-export default CourseFilter
+export default CourseFilter;
