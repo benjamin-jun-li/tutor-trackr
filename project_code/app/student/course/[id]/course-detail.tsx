db545f82454c2e77e1f7e3fa2a027fa19fd54b0d
Oliver Wu <z5379238@ad.unsw.edu.au>
2023-10-29 22:29:27 +1100

    course details bug fix

--- project_code/app/student/course/[id]/course-detail.tsx
+++ project_code/app/student/course/[id]/course-detail.tsx
@@ -10,9 +10,11 @@ import { useParams } from "next/navigation";
 import Link from "next/link";
 import { useQuery } from "@apollo/client";
 import { GET_COURSE } from "@/graphql/queries";
+import {useContextValue} from "@/components/context";
 
 const CourseDetail:FC<{ role:string }> = ({ role }) => {
     const params = useParams();
+    const { getters } = useContextValue();
     const getCourseDetail = useQuery(GET_COURSE, {variables:{ id:params?.id }});
     if (getCourseDetail.data)console.log(getCourseDetail.data);
     return (
@@ -22,7 +24,7 @@ const CourseDetail:FC<{ role:string }> = ({ role }) => {
                     <nav className="flex" aria-label="Breadcrumb">
                         <ol className="inline-flex items-center space-x-1 md:space-x-3">
                             <li className="inline-flex items-center">
-                                <Link href="/student/dashboard/" className="inline-flex items-center text-md font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                                <Link href={`/${getters.userIdentity}/dashboard/`} className="inline-flex items-center text-md font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
                                     <svg className="w-3 h-3 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                         <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
                                     </svg>
