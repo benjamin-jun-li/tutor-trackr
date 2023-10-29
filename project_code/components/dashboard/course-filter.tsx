f8d599dd1363615c2b3efd84b8c837126123e2eb
dior <cidawang24@gmail.com>
2023-10-29 22:07:25 +1100

    UOOI-20 update tutoradmin dashboard

--- project_code/components/dashboard/course-filter.tsx
+++ project_code/components/dashboard/course-filter.tsx
@@ -16,24 +16,12 @@ const experienceLevels = [
     { value: "expert", label: "Expert" },
 ];
 
-const courseCounts = [
-    { value: "1-10", label: "1-10 courses" },
-    { value: "11-50", label: "11-50 courses" },
-];
-
-const tutorRates = [
-    { value: "10-20", label: "$10-$20/hr" },
-    { value: "21-50", label: "$21-$50/hr" },
-];
-
 const CourseFilter = () => {
     return (
         <section style={{ display: 'flex'}}>
             <Combobox items={timeZones} placeholder="Select Time Zone" />
             <Combobox items={courseTypes} placeholder="Select Course Type" />
             <Combobox items={experienceLevels} placeholder="Select Tutor Experience Level" />
-            <Combobox items={courseCounts} placeholder="Select Tutor Course Count" />
-            <Combobox items={tutorRates} placeholder="Select Tutor Rate" />
         </section>
     );
 }
