0de474f6ed020de112baff9d7f81687d1cb07ce1
dior <cidawang24@gmail.com>
2023-10-30 00:03:19 +1100

    UOOI update applicationpage

--- project_code/app/admin/tutoradmin/application/[id]/page.tsx
+++ project_code/app/admin/tutoradmin/application/[id]/page.tsx
@@ -1,18 +1,42 @@
-"use client"
+"use client";
 import { GET_APPLICATION_BY_ID } from "@/graphql/queries";
-import {useQuery} from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { useParams } from "next/navigation";
 
 const ApplicationPage = () => {
-    const param = useParams()
-    const application = useQuery(GET_APPLICATION_BY_ID, {variables:{ id: param?.id}})
-    console.log(application);
+    const param = useParams();
+    const application = useQuery(GET_APPLICATION_BY_ID, { variables: { id: param?.id } });
+
+    const appDetails = application.data?.getSingleApplication;
+
+    if (!appDetails) {
+        return <div>Loading...</div>;
+    }
+
     return (
-        <main className="mt-[6rem]">
-            <h1>title</h1>
-        </main>
-    )
+        <div className="flex flex-col items-center mt-[6rem]">
+            <h1 className="text-3xl font-bold mb-6">{appDetails.name}'s Detail</h1>
+            <main className="p-5 bg-white shadow-lg rounded-lg w-full max-w-2xl">
+                <div className="mb-4">
+                    <span className="font-semibold">Name:</span> {appDetails.name}
+                </div>
+                <div className="mb-4">
+                    <span className="font-semibold">Email:</span> {appDetails.email}
+                </div>
+                <div className="mb-4">
+                    <span className="font-semibold">Course Name:</span> {appDetails.courseName}
+                </div>
+                <div className="mb-4">
+                    <span className="font-semibold">Description:</span> {appDetails.description}
+                </div>
+                {appDetails.interview && (
+                    <div className="mb-4">
+                        <span className="font-semibold">Interview:</span> {appDetails.interview}
+                    </div>
+                )}
+            </main>
+        </div>
+    );
 }
 
-export default ApplicationPage
-
+export default ApplicationPage;
