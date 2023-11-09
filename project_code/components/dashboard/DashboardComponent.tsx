b4c0e92d64880fec514dc7cd2cb8aa14b3953ce0
Jun Li <benjaminli060@gmail.com>
2023-11-09 17:20:42 +1100

    added imagekit.io for cdn support

--- project_code/components/dashboard/DashboardComponent.tsx
+++ project_code/components/dashboard/DashboardComponent.tsx
@@ -21,18 +21,14 @@ const DashboardComponent:FC<DashboardComponentProps> = ({ role, courseType}) => 
     <section className="space-y-4 py-4">
         <div className="mx-10">
           {role === "student" && courseType === "all" &&
-              <div className="flex justify-center items-center mb-4">
                 <CourseFilter onFilterChange={setFilters} />
-              </div>
           }
           <div className="place-items-center">
-            <div className="flex flex-col justify-center items-center">
-              { courseType === "all" ? (
-                  <AllCourseList role={role} courseType={courseType} filters={filters} />
-              ) : (
-                  <MyCourseList role={role} courseType={courseType}/>
-              )}
-            </div>
+            { courseType === "all" ? (
+                <AllCourseList role={role} courseType={courseType} filters={filters} />
+            ) : (
+                <MyCourseList role={role} courseType={courseType}/>
+            )}
           </div>
         </div>
     </section>
