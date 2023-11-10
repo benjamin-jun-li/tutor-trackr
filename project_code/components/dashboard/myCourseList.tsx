0d7234e0ab541614d424c353cd4997d5c2d15d88
Oliver Wu <z5379238@ad.unsw.edu.au>
2023-11-10 18:35:58 +1100

    test for my course

--- project_code/components/dashboard/myCourseList.tsx
+++ project_code/components/dashboard/myCourseList.tsx
@@ -1,33 +1,56 @@
 "use client"
-import { FC } from "react";
+import {FC, useEffect, useState} from "react";
 import { GET_TUTOR_COURSES, GET_STUDENT_COURSES } from "@/graphql/queries";
-import {useQuery} from "@apollo/client";
+import {useLazyQuery, useQuery} from "@apollo/client";
 import CourseListComponent from "@/components/dashboard/courseListComponent";
+import {useParams} from "next/navigation";
 
 interface MyCourseProps{
     role: string,
     courseType: string
 }
 const MyCourseList:FC<MyCourseProps> = ({role, courseType}) => {
-    if (role === "student") {
-        const {data, loading, error} = useQuery(GET_STUDENT_COURSES, {
-            variables:{
-                studentId: "6548da33d399acae11184312"
+    const params = useParams();
+    const [getStudentCourses, {data: studentCourses, loading: studentLoading, error: studentError}] = useLazyQuery(GET_STUDENT_COURSES);
+    const [getTutorCourses, {data: tutorCourses, loading: tutorLoading, error: tutorError}] = useLazyQuery(GET_TUTOR_COURSES);
+    const [data, setData] = useState<any>([]);
+
+    useEffect(() => {
+        // Define the async function inside useEffect
+        const fetchData = async () => {
+            if (role === "student") {
+                const data = await getStudentCourses({
+                    variables: {
+                        studentId: params?.userID
+                    }
+                });
+                // setData(data?.getStudentCourses);
+            } else {
+                const data = await getTutorCourses({
+                    variables: {
+                        tutorId: params?.userID
+                    }
+                });
+                console.log(data?.getTutorCourses)
+                // setData(data?.getTutorCourses);
             }
-        })
-        return (
-            <CourseListComponent data={data?.courses} role={role} courseType={courseType}/>
-        )
-    } else {
-        const {data, loading, error} = useQuery(GET_TUTOR_COURSES, {
-            variables:{
-                tutorId: "6548da33d399acae11184312"
-            }
-        })
-        return (
-            <CourseListComponent data={data?.courses} role={role} courseType={courseType}/>
-        )
-    }
+        };
+
+        // Call the async function
+        fetchData();
+
+        // If you have to clean up resources when the component
+        // unmounts or when the dependencies change, return a cleanup function
+        return () => {
+            // Clean up code here
+        };
+    }, [role, params?.userID, getStudentCourses, getTutorCourses]);
+
+    console.log(data)
+
+    return (
+        <CourseListComponent data={data} role={role} courseType={courseType}/>
+    )
 }
 
 export default MyCourseList
