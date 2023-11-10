3f6405ab52f2680e937d08b427d2f11c16cb70c6
Jun Li <benjaminli060@gmail.com>
2023-11-10 19:59:07 +1100

    fix logic

--- project_code/components/dashboard/myCourseList.tsx
+++ project_code/components/dashboard/myCourseList.tsx
@@ -1,48 +1,21 @@
-"use client"
-import {FC, useEffect, useState, useRef} from "react";
-import { GET_TUTOR_COURSES, GET_STUDENT_COURSES } from "@/graphql/queries";
-import {useLazyQuery, useQuery} from "@apollo/client";
-import CourseListComponent from "@/components/dashboard/courseListComponent";
-import {useParams} from "next/navigation";
+import {FC} from "react";
+import MyCourseStudent from "@/components/dashboard/myCourseStudent";
+import MyCourseTutor from "@/components/dashboard/myCourseTutor";
 
 interface MyCourseProps{
     role: string,
     courseType: string
 }
 const MyCourseList:FC<MyCourseProps> = ({role, courseType}) => {
-    const params = useParams();
-    const [getStudentCourses, {data: studentCourses, loading: studentLoading, error: studentError}] = useLazyQuery(GET_STUDENT_COURSES);
-    const [getTutorCourses, {data: tutorCourses, loading: tutorLoading, error: tutorError}] = useLazyQuery(GET_TUTOR_COURSES);
-    const data:any = useRef([]);
-    useEffect(() => {
-        if (role === "student") {
-            getStudentCourses({
-                variables: {
-                    studentId: params?.userID
-                }
-            }).then((res) => {
-                data.current = res;
-            });
-        } else {
-            getTutorCourses({
-                variables: {
-                    tutorId: params?.userID
-                }
-            }).then((res) => {
-                data.current = res;
-                console.log(res.data.getTutorCourses);
-            });
-
-        }
-    }, []);
-    // console.log(data);
-    return (
-        <></>
-    )
-
-    return (
-        <CourseListComponent data={data} role={role} courseType={courseType}/>
-    )
+    if (role === "student") {
+        return (
+            <MyCourseStudent role={role} courseType={courseType}/>
+        )
+    } else {
+        return (
+            <MyCourseTutor role={role} courseType={courseType}/>
+        )
+    }
 }
 
 export default MyCourseList
---
+++ project_code/components/dashboard/myCourseStudent.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { GET_STUDENT_COURSES } from "@/graphql/queries";
+import { useQuery} from "@apollo/client";
+import CourseListComponent from "@/components/dashboard/courseListComponent";
+import {useParams} from "next/navigation";
+import {FC} from "react";
+
+interface MCSprops {
+    role: string,
+    courseType: string,
+}
+const MyCourseStudent:FC<MCSprops> = ({role, courseType}) => {
+    const params = useParams();
+    const {data: studentCourses, loading: studentLoading, error: studentError} = useQuery(GET_STUDENT_COURSES, {
+        variables:{
+            studentId: params?.userID
+    }});
+    if (studentLoading) {
+        return <div>loading</div>
+    }
+    return (
+        <CourseListComponent data={studentCourses?.getStudentCourses} role={role} courseType={courseType}/>
+    )
+}
+
+export default MyCourseStudent;
