29ed7f253a74ac8588740b4bb1d240b2580fc922
Oliver Wu <z5379238@ad.unsw.edu.au>
2023-11-12 16:50:46 +1100

    AppointsDisplay.tsx

--- project_code/components/AppointsDisplay.tsx
+++ project_code/components/AppointsDisplay.tsx
@@ -9,19 +9,26 @@ import Link from "next/link";
 const AppointsDisplay = () => {
     const params = useParams();
     const { loading, error, data } = useQuery(GET_Appointment, {
-        variables: { id: params?.id },
+        variables: { id: params?.sessionID },
         fetchPolicy: 'network-only'
     });
+
+    console.log(data)
+
+    const formatDateTime = (dateTimeString: string) => {
+        const date = new Date(dateTimeString);
+        return date.toLocaleString();
+    }
 
     return (
         <div className="p-6 bg-white shadow-md rounded-lg space-y-6">
             <div className="text-gray-700">Course Name: {data?.getAppointmentById?.courseName}</div>
             <div className="text-gray-700">Student Email: {data?.getAppointmentById?.studentEmail}</div>
             <div className="text-gray-700">Student Name: {data?.getAppointmentById?.studentName}</div>
-            <div className="text-gray-700">Start Time: {data?.getAppointmentById?.startTime}</div>
-            <div className="text-gray-700">End Time: {data?.getAppointmentById?.endTime}</div>
+            <div className="text-gray-700">Start Time: {formatDateTime(data?.getAppointmentById?.startTime)}</div>
+            <div className="text-gray-700">End Time: {formatDateTime(data?.getAppointmentById?.endTime)}</div>
             <div className="text-gray-700">Status: {data?.getAppointmentById?.status}</div>
-            <Link href={`${params.userID}/tutor/dashboard`}>
+            <Link href={`/${params?.userID}/student/dashboard`}>
                 <Button className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">Back</Button>
             </Link>
         </div>
