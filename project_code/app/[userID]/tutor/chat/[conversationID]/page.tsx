d6e13ff86a9f8c7b7d9eba9383ec3be396e1907e
Jun Li <benjaminli060@gmail.com>
2023-11-12 23:52:16 +1100

    adjust layout

--- project_code/app/[userID]/tutor/chat/[conversationID]/page.tsx
+++ project_code/app/[userID]/tutor/chat/[conversationID]/page.tsx
@@ -10,7 +10,7 @@ const TutorChat = () => {
         <main className="mt-[5rem] mb-3 flex flex-col justify-between min-h-[80vh] mx-4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <section>
                 <SocketIndicator />
-                <ChatMessages className="mx-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                <ChatMessages className="mx-1 bg-white"
                               apiUrl={"/api/messages"}
                               socketUrl={"/api/socket/messages"}
                               conversationId={params?.conversationID}
